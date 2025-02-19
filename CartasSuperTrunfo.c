476fe893d72c66f724167741d6b168b55cb7a3c9
VItor252025 <codmw541@gmail.com>
2025-02-19 00:52:07 +0000

    super trunfo corrigido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAX_CARTAS 1
+#define MAX_CARTAS 2
 
 typedef struct {
     char estado[50];
@@ -11,26 +11,6 @@ typedef struct {
     float pib;
     int pontosTuristicos;
 } Carta;
-void cadastrarCartas(Carta cartas[1], int qtd) {
-    for (int i = 0; i < qtd; i++) {
-        printf("\nCadastro da carta %d:\n", i + 1);
-        printf("Estado:São Paulo ");
-        scanf(" %[^\n]", cartas[i].estado);
-        printf("Código da carta:SP01 ");
-        scanf(" %[^\n]", cartas[i].codigo);
-        printf("Nome da cidade:São Paulo ");
-        scanf(" %[^\n]", cartas[i].nomeCidade);
-        printf("População:12300000 ");
-        scanf("%d", &cartas[i].populacao);
-        printf("Área (em km²):1521.11 ");
-        scanf("%f", &cartas[i].area);
-        printf("PIB:699000.00 ");
-        scanf("%f", &cartas[i].pib);
-        printf("Número de pontos turísticos:10 ");
-        scanf("%d", &cartas[i].pontosTuristicos);
-    }
-}
-
 void exibirCartas(Carta cartas[], int qtd) {
     printf("\nCartas cadastradas:\n");
     for (int i = 0; i < qtd; i++) {
@@ -47,28 +27,13 @@ void exibirCartas(Carta cartas[], int qtd) {
 
 int main() {
     Carta cartas[MAX_CARTAS] = {
-        {"São Paulo", "SP01", "São Paulo", 12300000, 1521.11, 699000.00, 10} 
+        {"São Paulo", "SP01", "São Paulo", 12300000, 1521.11, 699000.00, 10},
+        {"Rio de Janeiro", "RJ02", "Rio de Janeiro", 6775561, 1200.27, 364000.50, 15}
     };
 
-    int qtdCartas = 1; 
-    int opcao;
+    exibirCartas(cartas, MAX_CARTAS);
 
-    printf("Deseja cadastrar mais cartas? (1 - Sim / 0 - Não): ");
-    scanf("%d", &opcao);
 
-    if (opcao == 1) {
-        int novasCartas;
-        printf("Quantas novas cartas deseja cadastrar (máximo %d)? ", MAX_CARTAS - qtdCartas);
-        scanf("%d", &novasCartas);
-
-        if (novasCartas > 0 && (qtdCartas + novasCartas) <= MAX_CARTAS) {
-            cadastrarCartas(&cartas[qtdCartas], novasCartas);
-            qtdCartas += novasCartas;
-        } else {
-            printf("Quantidade inválida!\n");
-        }
-    }
-    exibirCartas(cartas, qtdCartas);
 
     return 0;
        
